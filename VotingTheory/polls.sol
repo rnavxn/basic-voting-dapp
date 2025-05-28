06236d11178ffaecb0e35b76ec2ad10f7672c6d7
rnavxn <rnavxn@gmail.com>
2025-05-28 14:54:43 +0530

    Reduced the codes where it overkills, commented the getActivePolls() function since it feel heavy for basic dapp and there exists an easier ways to filter out in front-end. Added new param for getResult function as bool type to represent the poll status. Added a new function to get the total pool's count.

--- VotingTheory/polls.sol
+++ VotingTheory/polls.sol
@@ -11,25 +11,25 @@ contract Polls {
     // Structure of poll
     struct Poll {
         string title;
-        string[] options;   // To track options
+        bytes32[] options;   // To track options
         uint32 endTime;
-        mapping (string => uint256) votes;  // Option : Count
+        mapping (bytes32 => uint256) votes;  // Option : Count
         mapping (address => bool) voted;    // Address : Voted?
     }
 
-    struct ActivePolls {
-        string title;
-        string[] options;
-        uint32 endTime;
-        uint256 pollId;
-    }
+    // struct ActivePolls {
+    //     string title;
+    //     bytes32[] options;
+    //     uint32 endTime;
+    //     uint256 pollId;
+    // }
 
     // Struct-array to store the polls created
     Poll[] public polls;
-    // Poll[] private activePolls;
+    // mapping (uint256 => bool) public activePolls;
 
-    event PollCreated(string title, string[] options, uint256 duration);
-    event Voted(bytes);
+    event PollCreated(string title, bytes32[] options, uint256 duration);
+    event Voted(address voter, uint256 pollId);
 
     /// @dev To check whether the poll is active
     modifier onlyActive(uint256 _pollId) {
@@ -38,7 +38,7 @@ contract Polls {
     }
 
     /// @dev A function to create the poll in DApp
-    function createPoll(string memory _title, string[] memory _options, uint32 _duration) external returns (uint256) {
+    function createPoll(string memory _title, bytes32[] memory _options, uint32 _duration) external returns (uint256) {
         require (_options.length > 0, "No options provided");
         polls.push();
         uint256 pollId = polls.length - 1;
@@ -52,58 +52,71 @@ contract Polls {
     }
 
     /// @dev A fucntion to make the vote
-    function vote(uint256 _pollId, string memory _option) external onlyActive(_pollId) {
-        require (_pollId <= polls.length, "Invalid Poll ID");   // this line is not reflecting in log or as error yet instead smth abt payable error
+    function vote(uint256 _pollId, bytes32 _option) external onlyActive(_pollId) {
+        require (_pollId < polls.length, "Invalid Poll ID");   // this line is not reflecting in log or as error yet instead smth abt payable error
         Poll storage poll = polls[_pollId];
         require (!poll.voted[msg.sender], "Already voted");
         require (isValidOption(poll.options, _option), "Invalid option");
         poll.votes[_option]++;
         poll.voted[msg.sender] = true;
-        emit Voted(abi.encodePacked(msg.sender , " has voted."));
+        // emit Voted(abi.encodePacked(msg.sender , " has voted."));
+        emit Voted(msg.sender, _pollId);
     }
 
     /// @dev Afunction to validate the proposed voting option
-    function isValidOption(string[] memory _options, string memory _option) internal pure returns (bool) {
-        bytes32 optionHash = keccak256(bytes(_option));
+    function isValidOption(bytes32[] memory _options, bytes32 _option) internal pure returns (bool) {
         for(uint i = 0; i < _options.length; i++) {
-            if(keccak256(bytes(_options[i])) == optionHash) {
+            if(_options[i] == _option) {
                 return true;
             }
         }
         return false;
     }
 
+    /// @dev Afunction to get poll details
+    function getPoll(uint256 _pollId) external view returns(string memory, bytes32[] memory, uint32) {
+        Poll storage poll = polls[_pollId];
+        return (poll.title, poll.options, poll.endTime);
+    }
+
     /// @dev A function to retrieve the votes
-    function getResults(uint256 _pollId) external view returns (string[] memory, uint256[] memory) {
+    function getResults(uint256 _pollId) external view returns (bytes32[] memory, uint256[] memory, bool) {
         Poll storage poll = polls[_pollId];
         uint256[] memory counts = new uint256[](poll.options.length);
         for(uint i = 0; i < poll.options.length; i++) {
             counts[i] = poll.votes[poll.options[i]];
         }
-        return (poll.options, counts);
+        return (poll.options, counts, poll.endTime > block.timestamp);
     }
 
-    // @dev A function to retrieve the active polls
-    function getActivePolls() external view returns (ActivePolls[] memory) {
-        uint256 activeCount = 0;
-        for (uint i = 0; i < polls.length; i++) {
-            if (polls[i].endTime >= block.timestamp) {
-                activeCount++;
-            }
-        }
-        ActivePolls[] memory actPoll = new ActivePolls[](activeCount);
-        uint256 index = 0;
-        for(uint i = 0; i < polls.length; i++) {
-            if (polls[i].endTime >= block.timestamp) {
-                actPoll[index] = ActivePolls({
-                    pollId: i,
-                    title: polls[i].title,
-                    endTime: polls[i].endTime,
-                    options: polls[i].options
-                });
-                index++;
-            }
-        }
-        return actPoll;
+    /// @dev A function to get totla polls
+    function getTotalPolls() external view returns (uint256) {
+        return polls.length;
     }
+
+
+    /// @dev A function to retrieve the active polls
+    // function getActivePolls() external view returns (ActivePolls[] memory) {
+    //     uint256 activeCount = 0;
+    //     for (uint i = 0; i < polls.length; i++) {
+    //         if (polls[i].endTime >= block.timestamp) {
+    //             activeCount++;
+    //         }
+    //     }
+    //     ActivePolls[] memory actPoll = new ActivePolls[](activeCount);
+    //     uint256 index = 0;
+    //     for(uint i = 0; i < polls.length; i++) {
+    //         if (polls[i].endTime >= block.timestamp) {
+    //             actPoll[index] = ActivePolls({
+    //                 pollId: i,
+    //                 title: polls[i].title,
+    //                 endTime: polls[i].endTime,
+    //                 options: polls[i].options
+    //             });
+    //             index++;
+    //         }
+    //     }
+    //     return actPoll;
+    // }
+
 }
